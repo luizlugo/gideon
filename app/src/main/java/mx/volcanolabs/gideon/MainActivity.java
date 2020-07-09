81641e77ffb4876fd055f1ce05b4e54880188dbf
Luis Lugo <luis.lugo@shockoe.com>
2020-07-08 20:04:42 -0500

    adding authentication

--- app/src/main/java/mx/volcanolabs/gideon/MainActivity.java
+++ app/src/main/java/mx/volcanolabs/gideon/MainActivity.java
@@ -1,13 +1,26 @@
 package mx.volcanolabs.gideon;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 import mx.volcanolabs.gideon.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
+    // Firebase authentication
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private final int RC_SIGN_IN = 1903;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +29,68 @@ public class MainActivity extends AppCompatActivity {
         view = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(view.getRoot());
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         setupActionBar();
+        checkAuthenticatedUser();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN && resultCode == RESULT_CANCELED) {
+            finish();
+        }
     }
 
     private void setupActionBar() {
         setSupportActionBar(view.toolbar);
+    }
+
+    private void checkAuthenticatedUser() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // onSignInInitialized(user.getDisplayName());
+                } else {
+                    openSignInFlow();
+                }
+            }
+        };
+    }
+
+    private void openSignInFlow() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                        new AuthUI.IdpConfig.EmailBuilder().build()
+                                )
+                        )
+                        .build(),
+                RC_SIGN_IN
+        );
     }
 }
